4f15a5e5434b75b8c996a63a7be70863d5b58d4d
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-09-22 00:22:29 +0200

    Modifico codigo y estilos, 2 botones funcionando

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
-let pantalla = document.getElementById("pantallaNegra");
+let pantalla = document.getElementById("screen");
 
 document.addEventListener("click", (e)=>{
 
-
-    if(e.target.id === 'Jose'){
-
-        // pantalla.style.backgroundColor = "red";
-        pantalla.style.backgroundImage = "url('img/kawai.jpg')";
-
-    } else if (e.target.id === 'paco') {
-
-        // pantalla.style.backgroundColor = "grey";
-        pantalla.style.backgroundImage = "url('img/bear.jpg')";
-
-    };
+switch (e.target.id){
+    case 'botonIzquierdo':
+       pantalla.style.backgroundImage = "url('imagenes/TetrisCabecera.jpg')"; break;
+    case 'botonDerecho':
+       pantalla.style.backgroundImage = "url('imagenes/tetrisJuego.jfif')"; break;
+    case 'botonApagado':
+      pantalla.style.backgroundColor = "pantallaCentral"; break;
+    default :
+       }
+    
 
 
+
+//
+ //  if(e.target.id === 'botonIzquierdo'){
+ //       pantalla.style.backgroundImage = "url('imagenes/TetrisCabecera.jpg')";
+//   } 
+ //  else if (e.target.id === 'botonDerecho'){
+ //       pantalla.style.backgroundImage = "url('imagenes/tetrisJuego.jfif')";
+ //  }
+ //  else if (e.target.id === 'botonApagado'){
+ //       pantalla.style.backgroundColor = "black";}
+
+//
 });
