109e268f09a5c1dc20514492bebc3c22c46b839b
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-09-26 12:03:28 +0200

    Cambio nombre imagenes de main

--- js/main.js
+++ js/main.js
@@ -4,18 +4,18 @@ let encendido = 0;
 document.addEventListener("click", (e) => {
 
   if (encendido == 0 && e.target.id == 'botonApagado') {
-    pantalla.style.backgroundImage = "url('./imagenes/Nintendo-Switch.jpg";
+    pantalla.style.backgroundImage = "url('./IMAGENES/Nintendo-Switch.jpg";
     encendido = 1;
   }
   else if (encendido == 1) {
     switch (e.target.id) {
 
       case 'botonIzquierdo':
-        pantalla.style.backgroundImage = "url('./imagenes/CabeceraMario.jpg')"; 
+        pantalla.style.backgroundImage = "url('./IMAGENES/CabeceraMario.jpg')"; 
         break;
 
       case 'botonDerecho':
-        pantalla.style.backgroundImage = "url('./imagenes/MarioJugando.jpg')";
+        pantalla.style.backgroundImage = "url('./IMAGENES/MarioJugando.jpg')";
         break;
 
       case 'botonApagado':
